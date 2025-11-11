a3a6d7c76facab3a6c6d7368a2054f4f4dd8278e
Luis Lobaton <luislobatonmendoza@gmail.com>
2025-11-11 14:53:15 -0500

    Delete IaC/database.tf

--- IaC/database.tf
+++
@@ -1,9 +0,0 @@
-resource "aws_instance" "database" {
-  ami           = "ami-xxxxxxxx"
-  instance_type = "t2.micro"
-  subnet_id     = aws_subnet.public.id
-
-  tags = {
-    Name = "database"
-  }
-}
